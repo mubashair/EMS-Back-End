96018d70eb2180dc0627d534476ec49d9f61a98a
Mubashar <mubashair@gmail.com>
2024-08-31 18:27:24 +0500

    ems backend is being developed

---
+++ src/main/java/com/prog/ems/entity/Employee.java
@@ -0,0 +1,54 @@
+package com.prog.ems.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity(name = "employees")
+public class Employee {//map java objects to tables in rdbms
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String firstName;
+	private String lastName;
+	@Column(name="email_id", nullable = false, unique = true)
+	private String email;
+	
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
+	
+	
+	
+	
+	
+
+}
